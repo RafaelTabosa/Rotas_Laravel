b59cd5b360db9d7632c7a52a963cf163d7c2003e
RafaelTabosa <111073214+RafaelTabosa@users.noreply.github.com>
2023-11-30 20:21:50 -0300

    Initial commit

---
+++ app/Http/Controllers/ContatoController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ContatoController extends Controller {
+    public function index() {
+        return view('contato.index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
+
+Route::get('/', [PrincipalController::class, 'index'])->name('principal');
+Route::get('/sobre-nos', [SobreNosController::class, 'index'])->name('sobre-nos');
+Route::get('/contato', [ContatoController::class, 'index'])->name('contato');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
